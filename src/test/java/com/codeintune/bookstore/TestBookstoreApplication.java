6a10d74c1cc642d8cd0832026e9233d88aa1a6c6
Nicola Rossi <ncl.rss.92@gmail.com>
2025-12-08 12:26:41 +0100

    Basic setup of project

---
+++ src/test/java/com/codeintune/bookstore/TestBookstoreApplication.java
@@ -0,0 +1,11 @@
+package com.codeintune.bookstore;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(BookstoreApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
