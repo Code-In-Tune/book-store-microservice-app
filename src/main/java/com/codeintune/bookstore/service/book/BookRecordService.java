1a4bb555a9b95d1683c448c72d40a4325c1b772d
Nicola Rossi <ncl.rss.92@gmail.com>
2025-12-16 20:51:54 +0100

    Added BookRecordService interface

---
+++ src/main/java/com/codeintune/bookstore/service/book/BookRecordService.java
@@ -0,0 +1,24 @@
+package com.codeintune.bookstore.service.book;
+
+import com.codeintune.bookstore.dto.book.request.SaveBookRecordRequestDTO;
+import com.codeintune.bookstore.dto.book.request.UpdateBookRecordByIdRequestDTO;
+import com.codeintune.bookstore.dto.book.request.UpdateBookRecordQuantityRequestDTO;
+import com.codeintune.bookstore.dto.book.response.GetBookRecordByIdResponseDTO;
+import com.codeintune.bookstore.dto.book.response.GetBookRecordsResponseDTO;
+import com.codeintune.bookstore.dto.book.response.SaveBookRecordResponseDTO;
+import com.codeintune.bookstore.dto.book.response.UpdateBookRecordByIdResponseDTO;
+import com.codeintune.bookstore.dto.filter.SearchBookRecordsDTO;
+import org.springframework.data.domain.PageRequest;
+
+public interface BookRecordService {
+
+    SaveBookRecordResponseDTO saveBookRecord(SaveBookRecordRequestDTO saveBookRecordRequestDTO);
+    GetBookRecordByIdResponseDTO getBookRecordById(Long bookId);
+    UpdateBookRecordByIdResponseDTO updateBookRecordById(UpdateBookRecordByIdRequestDTO updateBookRecordByIdRequestDTO);
+    UpdateBookRecordByIdResponseDTO updateBookRecordQuantity(UpdateBookRecordQuantityRequestDTO updateBookRecordQuantityRequestDTO);
+    void removeBookRecordById(Long bookId);
+    GetBookRecordsResponseDTO getAllBookRecords(PageRequest pageRequest);
+    GetBookRecordsResponseDTO getBookRecordsByAuthor(PageRequest pageRequest, SearchBookRecordsDTO searchBookRecordsDTO);
+    GetBookRecordsResponseDTO getBookRecordsByTitle(PageRequest pageRequest, SearchBookRecordsDTO title);
+    GetBookRecordsResponseDTO getBookRecordsByPublisher(PageRequest pageRequest, SearchBookRecordsDTO searchBookRecordsDTO);
+}
